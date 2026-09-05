d980bbe0c6a734ddfc2c92051e06e9763c9ebd97
Shirisha3141 <mandhasirisha90@gmail.com>
2026-09-06 00:34:01 +0530

    Deploy portfolio to shirisha_protfolio repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/devops-journey/',
+  base: '/shirisha_protfolio/',
   server: {
     port: 3000,
     open: false
